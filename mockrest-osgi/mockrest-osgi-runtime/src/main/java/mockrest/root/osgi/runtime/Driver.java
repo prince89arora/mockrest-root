c1e417db958fd072146a5b7dbd9d45b77a6109b3
Prince Arora <prnc.arora0@gmail.com>
2019-08-21 16:21:53 +0530

    initial start and stop framework

--- mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
@@ -1,7 +1,12 @@
 package mockrest.root.osgi.runtime;
 
+import org.apache.commons.lang3.StringUtils;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ServiceLoader;
 
 /**
@@ -17,5 +22,30 @@ public class Driver {
         ServiceLoader<FrameworkFactory> frameworkFactory = ServiceLoader.load(FrameworkFactory.class);
         FrameworkFactory factory = frameworkFactory.iterator().next();
         System.out.println(factory);
+        Map<String, String> properties = new HashMap<>();
+        final Framework framework = factory.newFramework(properties);
+
+        try {
+            framework.start();
+        } catch (BundleException e) {
+            e.printStackTrace();
+        }
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            framework.start();
+                            System.out.println("Waiting for framework to stop..");
+                            framework.waitForStop(0);
+                            System.out.println("Framework stopped");
+                            System.exit(0);
+                        } catch (BundleException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
     }
 }
