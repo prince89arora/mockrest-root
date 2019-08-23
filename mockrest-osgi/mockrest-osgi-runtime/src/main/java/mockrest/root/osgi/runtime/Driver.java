cc8bc37ff9552079af24290a3d20f438a35f650d
Prince Arora <prnc.arora0@gmail.com>
2019-08-23 14:36:35 +0530

    logic to install core bundles

--- mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
@@ -1,12 +1,20 @@
 package mockrest.root.osgi.runtime;
 
+import org.apache.commons.lang3.StringUtils;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ServiceLoader;
@@ -31,12 +39,13 @@ public class Driver {
         ServiceLoader<FrameworkFactory> frameworkFactory = ServiceLoader.load(FrameworkFactory.class);
         FrameworkFactory factory = frameworkFactory.iterator().next();
         Map<String, String> properties = new HashMap<>();
-        final Framework framework = factory.newFramework(properties);
+        Framework framework = factory.newFramework(properties);
         logger.info("Initializing framework => "+ framework.getSymbolicName() + " - " + framework.getVersion());
 
         try {
             framework.start();
-        } catch (BundleException e) {
+            this.installConsole(framework);
+        } catch (BundleException | IOException e) {
             logger.error("Unable to start OSGI framework : ", e);
         }
 
@@ -58,5 +67,26 @@ public class Driver {
         );
 
         Runtime.getRuntime().addShutdownHook(closeHook);
+    }
+
+    private void installConsole(Framework framework) throws IOException {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("default_bundles");
+        System.out.println("original:: "+ url.getPath());
+        System.out.println("fetching:: "+ StringUtils.substringAfter(url.getPath(), "file:"));
+        System.out.println(
+                "Files: " + new File(StringUtils.substringAfter(url.getPath(), "file:")).isDirectory()
+        );
+        File[] files = new File(StringUtils.substringAfter(url.getPath(), "file:")).listFiles();
+
+        for (File file : files) {
+            logger.info("Processing bundle : "+ file.toURI().toString());
+            try {
+                Bundle bundle = framework.getBundleContext().installBundle(file.toURI().toString());
+                logger.info("Bundle installed: "+ bundle.getSymbolicName());
+                bundle.start();
+            } catch (BundleException e) {
+                logger.error("Unable to install bundle: ", e);
+            }
+        }
     }
 }
