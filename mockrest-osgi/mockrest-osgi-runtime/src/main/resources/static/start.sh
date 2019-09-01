a3643d702c06f77b7b713882ad9a82d56ec6aeb4
Prince Arora <prnc.arora0@gmail.com>
2019-09-01 11:10:00 +0530

    start and stop script files

--- mockrest-osgi/mockrest-osgi-runtime/src/main/resources/static/start.sh
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/resources/static/start.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo 'Starting MockRest'
+mr_version="1.0-SNAPSHOT";
+jvm_version="$(java -version 2>&1 | head -n 1)"
+
+echo "MockRest version: $mr_version"
+echo "Using JVM: $jvm_version"
+
+echo "_______  _____  _______ _     _  ______ _______ _______ _______"
+echo "|  |  | |     | |       |____/  |_____/ |______ |______    |"
+echo "|  |  | |_____| |_____  |    \_ |    \_ |______ ______|    |  "
