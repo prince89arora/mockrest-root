dd05e99a3a45ef8ebcd846d9406c1d7496e2e384
Prince Arora <prnc.arora0@gmail.com>
2019-09-03 12:51:41 +0530

    start script for initialisation

--- mockrest-osgi/mockrest-osgi-runtime/src/main/resources/static/start.sh
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/resources/static/start.sh
@@ -2,10 +2,17 @@
 
 mr_version="1.0-SNAPSHOT";
 jvm_version="$(java -version 2>&1 | head -n 1)"
+mr_dir="mockrest"
 
 echo "MockRest version: $mr_version"
 echo "Using JVM: $jvm_version"
-
+echo ""
 echo "_______  _____  _______ _     _  ______ _______ _______ _______"
 echo "|  |  | |     | |       |____/  |_____/ |______ |______    |"
 echo "|  |  | |_____| |_____  |    \_ |    \_ |______ ______|    |  "
+
+echo ""
+cd ../../
+confFile="$mr_dir"/conf/conf.properties
+executorFileName=$(cat "$confFile" | grep mr_executor | cut -d'=' -f2)
+java -jar "$executorFileName"
