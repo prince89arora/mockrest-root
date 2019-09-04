b05ce634bdcfdb01da5e4730afec388bc785d8cc
Prince Arora <prnc.arora0@gmail.com>
2019-09-04 22:58:21 +0530

    moved osgi startup in osgi factory

--- mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/Driver.java
@@ -1,20 +1,9 @@
 package mockrest.root.osgi.runtime;
 
-import org.osgi.framework.BundleException;
-import org.osgi.framework.launch.Framework;
-import org.osgi.framework.launch.FrameworkFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
-import java.util.ServiceLoader;
 
 /**
  * @author prince.arora
@@ -27,71 +16,12 @@ public class Driver {
         /*
         Init files structure setup for mockrest.
          */
-        MockRestFileStructure.getStructure().initSetup();
-        new Driver().init();
-    }
-
-    public static void exit() {
-        Runtime.getRuntime().exit(0);
-    }
-
-    public void init() {
-        ServiceLoader<FrameworkFactory> frameworkFactory = ServiceLoader.load(FrameworkFactory.class);
-        FrameworkFactory factory = frameworkFactory.iterator().next();
-        final Framework framework = factory.newFramework(this.getOsgiProperties());
-        logger.info("Initializing framework => "+ framework.getSymbolicName() + " - " + framework.getVersion());
-
         try {
-            framework.start();
-            MockRestContextProvider.init(framework);
+            MockRestFileStructure.getStructure().initSetup();
+            OSGiFrameworkFactory.init();
             MockRestStartupHelper.initSetup();
-        } catch (BundleException | IOException e) {
-            logger.error("Unable to start OSGI framework : ", e);
+        } catch (Exception ex) {
+            logger.error("Exception in Driver ", ex);
         }
-
-        /**
-         * close hook to shutdown OSGI framework.
-         */
-        Thread closeHook = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            framework.stop();
-                            framework.waitForStop(0);
-                            logger.info("Stopped framework : " + framework.getSymbolicName() + " - " + framework.getVersion());
-                            Driver.exit();
-                        } catch (BundleException | InterruptedException e) {
-                            logger.error("Unable to stop OSGI framework : ", e);
-                        }
-                    }
-                }
-        );
-        Runtime.getRuntime().addShutdownHook(closeHook);
-    }
-
-    /**
-     * Get properties from {@link MockRestFileStructure#getConfigurationDirectoryPath()} file.
-     * convert properties to {@link Map} to be passed to framework.
-     *
-     * @return Map<String, String>
-     */
-    private Map<String, String> getOsgiProperties() {
-        Map<String, String> map = new HashMap<>();
-        try (InputStream stream = new FileInputStream(MockRestFileStructure.getStructure()
-                .getOsgiConfigurationFilePath())) {
-            Properties properties = new Properties();
-            properties.load(stream);
-            //Add each property found in configuration properties file in map.
-            properties.stringPropertyNames().stream().forEach(key -> {
-                if (Objects.nonNull(properties.getProperty(key)) &&
-                        !properties.getProperty(key).isEmpty()) {
-                    map.put(key, properties.getProperty(key));
-                }
-            });
-        } catch (IOException e) {
-            logger.error("Error while preparing osgi properties: ", e);
-        }
-        return map;
     }
 }
--- mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/MockRestStartupHelper.java
+++ mockrest-osgi/mockrest-osgi-runtime/src/main/java/mockrest/root/osgi/runtime/MockRestStartupHelper.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.lang.management.ManagementFactory;
 import java.net.URISyntaxException;
